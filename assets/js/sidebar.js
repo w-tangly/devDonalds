3289f95ad3e7c907238bbe33ff7061d7431094fd
pedrh-carmo <pedro.campos.1bmangal@gmail.com>
2025-06-16 16:49:19 +0000

    .

---
+++ assets/js/sidebar.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "80vw";
+  document.getElementById("mySidepanel").style.height = "100vh";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
